603bd570e832da7ce975843cdf162df9eb7ab735
Sudeep Swain <sudeepswain2000@gmail.com>
2021-01-14 12:37:22 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
+/*Console Designing Style. */
+int Console(){
+
+}
+/*The position where the required symbols are to be inputed. */
+int InputKeys(){
+
+}
+/*How's the player gonna win? */
+int WinLogic(){
+
+}
+/**/
+int ToogleSymbol(){
+
+}
+
 int main() {
   
 return 0;
+  
 }
